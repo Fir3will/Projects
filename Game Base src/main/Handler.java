3452a64ba105853a579f4f3aae8d53fd5b96b465
Fir3will <kayanihashim@gmail.com>
2017-05-24 22:30:39 -0400

    Update 1

--- Game Base src/main/Handler.java
+++ Game Base src/main/Handler.java
@@ -1,3 +1,17 @@
+/**************************************************************************
+ *
+ * [2017] Fir3will, All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of "Fir3will" and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to "Fir3will" and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from "Fir3will".
+ **************************************************************************/
 package main;
 
 import java.awt.event.KeyEvent;
